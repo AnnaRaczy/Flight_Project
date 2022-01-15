2693a67aca4e3f44882507669583cc923cae7ba9
AnnaRaczy <38297862+AnnaRaczy@users.noreply.github.com>
2022-01-15 21:11:15 +0100

    Validation

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import useVisibleComponent from "../hooks/Visible";
+import { schemaSignup } from "./validation";
+import { useForm } from "react-hook-form";
 
 import {
   Button,
@@ -10,12 +12,18 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export function TextInput({ fn, label, name, type }) {
+const ErrorMessage = (nameValue) => {
+  return <p>{`errors.${nameValue}?.message`}</p>;
+};
+
+export function TextInput({ register, fn, label, name, type }) {
   return (
     <TextField
       className="selections_all--inputs login_input"
       variant="outlined"
+      ref={register}
       onChange={fn}
       label={label}
       name={name}
@@ -26,7 +34,7 @@ export function TextInput({ fn, label, name, type }) {
 
 const SignupBtn = ({ fn }) => {
   return (
-    <Button className="login_btn" onClick={fn}>
+    <Button className="login_btn" onSubmit={fn}>
       <Typography variant="button">Create Account</Typography>
     </Button>
   );
@@ -47,18 +55,28 @@ export function SignupForm({
     passwordConfirm: "",
   });
 
+  // to connect it with yup, pass a yup resolver containing created schema
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schemaSignup),
+  });
+  //register = fn(determines which fields are used for validation)
+  // handle = fn(as onSubmit of the form)
+  //errors = obj(constains all errors dispalyed by yup and stored here)
+
   const handleForm = (e) => {
+    const { name, value } = e.target;
     setState({
       ...state,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
   };
 
-  const handleSubmit = (e) => {
-    if (state.password !== state.passwordConfirm) {
-      console.log("Passwords do not match");
-    }
-    signUserUp(state.name, state.email, state.password).catch((err) =>
+  const submitForm = (data) => {
+    console.log(data.name);
+    // if (state.password !== state.passwordConfirm) {
+    //   console.log("Passwords do not match");
+    // }
+    signUserUp(data.name, data.email, data.password).catch((err) =>
       console.log(JSON.stringify(err))
     ); // Handle errors here.
   };
@@ -77,27 +95,44 @@ export function SignupForm({
       >
         <DialogTitle className="Header">Sign up</DialogTitle>
         <DialogContent>
-          <TextInput label="Name..." name="name" type="text" fn={handleForm} />
+          <TextInput
+            label="Name..."
+            name="name"
+            type="text"
+            // fn={handleForm}
+            register={register} //to be part of validation
+          />
+          {/* <ErrorMessage nameValue="name" /> */}
+          <p>{errors.name?.message}</p>
           <TextInput
             label="Email..."
             name="email"
             type="text"
-            fn={handleForm}
+            // fn={handleForm}
+            register={register}
           />
+          {/* <ErrorMessage nameValue="email" /> */}
+          <p>{errors.email?.message}</p>
           <TextInput
             label="Password..."
             name="password"
             type="password"
-            fn={handleForm}
+            // fn={handleForm}
+            register={register}
           />
+          {/* <ErrorMessage nameValue="password" /> */}
+          <p>{errors.password?.message}</p>
           <TextInput
             label="Password Confirmation..."
             name="passwordConfirm"
             type="password"
-            fn={handleForm}
+            // fn={handleForm}
+            register={register}
           />
+          {/* <ErrorMessage nameValue="passwordConfirm" /> */}
+          <p>{errors.passwordConfirm && "Password don't match"}</p>
           <div className="login_btn--wrapper">
-            <SignupBtn fn={handleSubmit} />
+            <SignupBtn fn={handleSubmit(submitForm)} />
             <BackBtn fn={handleBack} />
           </div>
         </DialogContent>
